fe9c8a74db51c3f26b435a3805478b74ec8a70a0
Michael Czechowski <m.czechowski@pons.de>
2024-04-11 00:07:54 +0200

    add room class add create, join, leave room

--- index.js
+++ index.js
@@ -1,32 +1,95 @@
-import { WebSocketServer } from "ws";
-
-const port = process.env.PORT || 8082;
+import {WebSocketServer} from "ws";
+import {port} from "./config/constants.js";
 
 const wss = new WebSocketServer({ port });
 
+class Room {
+    constructor(passcode) {
+        this.passcode = passcode;
+        this.context = {};
+        this.clients = new Set();
+    }
+
+    join(ws) {
+        this.clients.add(ws);
+        ws.send(JSON.stringify({ type: "joined-room", clients: this.clients.size, passcode: this.passcode }));
+    }
+
+    leave(ws) {
+        ws.send(JSON.stringify({ type: 'left-room', clients: this.clients.size, passcode: this.passcode }));
+        this.broadcast({ type: 'user-left', clients: this.clients.size });
+        this.clients.delete(ws);
+    }
+
+    broadcast(data) {
+        this.clients.forEach(client => client.send(JSON.stringify(data)));
+    }
+
+    updateContext(update) {
+        this.context = { ...this.context, ...update };
+    }
+}
+
+const rooms = new Map();
+
 wss.on("connection", (ws) => {
-    console.log("Some stranger connected");
-    ws.send("Welcome stranger!");
+    const send = (message) => ws.send(JSON.stringify(message));
+    ws.send(JSON.stringify({ type: 'connected', rooms: Array.from(rooms.keys()) }));
 
-    ws.on("message", (message) => {
-        console.log(`Received message: ${message}`);
+    // {"type":"create-room"}
+    // {"type":"rooms"}
+    // {"type":"join-room","passcode":"sk62"}
+    // {"type":"leave-room","id":"aDV6eQ=="}
 
-        const messageString = message.toString();
+    ws.on("message", async (message) => {
+        const event = JSON.parse(message);
 
-        switch (messageString) {
-            case "yala":
-                ws.send("Yala yala!");
+        if (!event) throw new Error('Invalid event');
+
+        switch (event.type) {
+            case 'rooms':
+                ws.send(JSON.stringify({ type: 'rooms', rooms: Array.from(rooms.keys()) }));
                 break;
-            case "sgeht":
-                ws.send("Alles gut!");
+            case "create-room":
+                let passcode = Math.random().toString(36).substring(2, 6);
+                let id = Buffer.from(passcode).toString("base64");
+                let newRoom = new Room(passcode);
+                rooms.set(id, newRoom);
+                newRoom.join(ws);
+                break;
+            case 'join-room':
+                let encodedPasscode = Buffer.from(event?.passcode).toString("base64");
+                let room = rooms.get(encodedPasscode);
+                if (room) {
+                    room.join(ws);
+                    room.broadcast({ type: 'user-joined', clients: room.clients.size });
+                } else {
+                    ws.send(JSON.stringify({ type: "error", message: `room with passcode ${event?.passcode} not found` }));
+                }
+                break;
+            case 'leave-room':
+                const roomToLeave = rooms.get(event?.id);
+                if (roomToLeave) {
+                    roomToLeave.leave(ws);
+                } else {
+                    ws.send(JSON.stringify({ type: "error", message: `room with id ${event?.id} not found` }));
+                }
+                break;
+            case 'update-context':
+                const roomToUpdate = rooms.get(event?.id);
+                if (roomToUpdate) {
+                    roomToUpdate.updateContext(event.context);
+                    roomToUpdate.broadcast({ type: 'context-updated', context: roomToUpdate.context });
+                } else {
+                    ws.send(JSON.stringify({ type: "error", message: `room with id ${event?.id} not found` }));
+                }
                 break;
             default:
-                ws.send("I don't understand you!");
+                ws.send(JSON.stringify({ type: 'error', message: 'Invalid event type' }));
         }
     });
 
-    // Handle disconnection
     ws.on('close', () => {
-        console.log('Schee wars');
+        rooms.forEach(room => room.leave(ws));
     });
 });
