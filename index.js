85039565c7ec59d853e7a95bd7268207b81ba40a
Michael Czechowski <m.czechowski@pons.de>
2024-04-11 21:58:42 +0200

    restore code

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ import {v4 as uuid} from "uuid";
 import {WebSocketServer} from "ws";
 import {port} from "./src/config/constants.js";
 import {currentRoom} from "./src/util/currentRoom.js";
+import {Room} from "./src/Room.js";
 
 const wss = new WebSocketServer({port});
 
 const rooms = new Map();
 const clients = new Map();
 
-
 wss.on("connection", (ws) => {
-	const clientId = uuid();
 	const send = (message) => ws.send(JSON.stringify(message));
+	const clientId = uuid();
 
-	console.log("Client connected", clientId);
+	clients.set(clientId, ws);
+
+	send({type: "connected", clientId, rooms: [...rooms.keys()]});
+
+	console.log(`Client connected with id ${clientId}`);
 
 	ws.on("message", async (message) => {
 		try {
@@ -22,39 +26,100 @@ wss.on("connection", (ws) => {
 			switch (event.type) {
 				// {"type":"room","message":"hello, world!"}
 				case "room":
-					console.log(`Recieved message "room"`);
+					currentRoom(clientId).clients.forEach(client => clients.get(client).send(JSON.stringify({
+						type: "room",
+						id: currentRoom(clientId).id,
+						message: event?.message,
+						from: client
+					})));
+					console.log(`Broadcasted message from ${clientId} to room ${currentRoom(clientId).id}:`, event?.message);
 					break;
 				// {"type":"all","message":"hello, world!"}
 				case "all":
-					console.log(`Recieved message "all"`);
+					clients.forEach(client => client.send(JSON.stringify({
+						type: "all",
+						message: event?.message,
+						from: clientId
+					})));
+					console.log(`Broadcasted message from ${clientId} to all:`, event?.message);
 					break;
 				case "ping":
-					console.log(`Recieved message "ping"`);
 					// {"type":"ping"}
+					send({type: "pong", clientId, room: currentRoom(clientId)});
 					break;
 				// {"type":"clients"}
 				case "clients":
-					console.log(`Recieved message "clients"`);
+					send({type: "clients", clients: Array.from(clients.keys())});
 					break;
 				// {"type":"rooms"}
 				case "rooms":
-					console.log(`Recieved message "rooms"`);
+					send({type: "rooms", rooms: Array.from(rooms.keys())});
 					break;
 				// {"type":"create-room"}
 				case "create-room":
-					console.log(`Recieved message "create-room"`);
+					let newRoom = new Room();
+					rooms.set(newRoom.id, newRoom);
+					send({type: "created-room", id: newRoom.id, passcode: newRoom.passcode});
+					clients.forEach(client => client.send(JSON.stringify({type: "room-created", id: newRoom.id})));
+					newRoom.join(clientId, ws);
+					console.log(`Room created with id ${newRoom.id} and passcode ${newRoom.passcode}`);
+					// console.debug(`{"type":"join-room","passcode":"${newRoom.passcode}"}`);
 					break;
 				// {"type":"join-room","passcode":"sk62"}
 				case "join-room":
-					console.log(`Recieved message "join-room"`);
+					let encodedPasscode = Buffer.from(event?.passcode).toString("base64");
+					let room = rooms.get(encodedPasscode);
+					if (room) {
+						room.join(clientId, ws);
+						room.broadcast({type: "user-joined", clients: room.clients.size});
+						console.log(`Client joined room ${room.id} with passcode ${event?.passcode}`);
+						// console.debug(`{"type":"leave-room","id":"${room.id}"}`);
+					} else {
+						ws.send(JSON.stringify({
+							type: "error",
+							message: `room with passcode ${event?.passcode} not found`
+						}));
+					}
 					break;
 				// {"type":"leave"}
 				case "leave":
-					console.log(`Recieved message "leave"`);
+					let currentRoomToLeave = currentRoom(clientId);
+					if (currentRoomToLeave) {
+						currentRoomToLeave.leave(clientId);
+					} else {
+						ws.send(JSON.stringify({type: "error", message: `room with id ${event?.id} not found`}));
+					}
+					break;
+				case "leave-room":
+					let roomToLeave = rooms.get(event?.id);
+					if (roomToLeave) {
+						roomToLeave.leave(clientId);
+					} else {
+						ws.send(JSON.stringify({type: "error", message: `room with id ${event?.id} not found`}));
+					}
 					break;
 				// {"type":"update","context":{"winner":"firat"}}
 				case "update":
-					console.log(`Recieved message "update"`);
+					let roomId = currentRoom(clientId)?.id;
+					let currentRoomToUpdate = rooms.get(roomId);
+					if (currentRoomToUpdate) {
+						currentRoomToUpdate.updateContext(event.context);
+						currentRoomToUpdate.broadcast({type: "context-updated", room: roomId, context: roomToUpdate.context});
+					} else {
+						ws.send(JSON.stringify({
+							type: "error",
+							message: `room with id ${currentRoom(clientId)?.id} with clientId ${clientId} not found`
+						}));
+					}
+					break;
+				case "update-context":
+					let roomToUpdate = rooms.get(event?.id);
+					if (roomToUpdate) {
+						roomToUpdate.updateContext(event.context);
+						roomToUpdate.broadcast({type: "context-updated", context: roomToUpdate.context});
+					} else {
+						ws.send(JSON.stringify({type: "error", message: `room with id ${event?.id} not found`}));
+					}
 					break;
 				default:
 					ws.send(JSON.stringify({type: "error", message: "Invalid event type"}));
@@ -65,7 +130,9 @@ wss.on("connection", (ws) => {
 	});
 
 	ws.on("close", () => {
-		console.log(`Client disconnected ${clientId}`);
+		rooms.forEach(room => room.leave(clientId));
+		clients.delete(clientId);
+		console.log(`Client left ${clientId}`);
 	});
 
 	ws.on("error", () => {
