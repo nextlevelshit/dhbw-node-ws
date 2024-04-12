cf87a81f50f37391f1cac49681ae928cd3a07e73
Michael Czechowski <m.czechowski@pons.de>
2024-04-12 13:00:22 +0200

    add jest globals fix code style

--- index.test.js
+++ index.test.js
@@ -1,74 +1,75 @@
-import WebSocket from 'ws';
+import WebSocket from "ws";
 import {port} from "./src/config/constants";
+import {describe, beforeEach, afterEach, test} from "@jest/globals";
 
-describe('WebSocket server', () => {
-    let client;
+describe("WebSocket server", () => {
+	let client;
 
-    beforeEach(() => {
-        client = new WebSocket(`ws://localhost:${port}`);
-    });
+	beforeEach(() => {
+		client = new WebSocket(`ws://localhost:${port}`);
+	});
 
-    afterEach(() => {
-        client.close();
-    });
+	afterEach(() => {
+		client.close();
+	});
 
-    test('should connect to the server', (done) => {
-        client.on('open', () => {
-            done()
-        });
-    });
+	test("should connect to the server", (done) => {
+		client.on("open", () => {
+			done()
+		});
+	});
 
-    test('should create a room when receiving a create-room event', (done) => {
-        client.on('message', (message) => {
-            const event = JSON.parse(message);
-            if (event.type === 'room-created') {
-                expect(event.id).toBeDefined();
-                done();
-            }
-        });
+	test("should create a room when receiving a create-room event", (done) => {
+		client.on("message", (message) => {
+			const event = JSON.parse(message);
+			if (event.type === "room-created") {
+				expect(event.id).toBeDefined();
+				done();
+			}
+		});
 
-        client.on('open', () => {
-            client.send(JSON.stringify({ type: 'create-room' }));
-        });
-    });
+		client.on("open", () => {
+			client.send(JSON.stringify({type: "create-room"}));
+		});
+	});
 
-    test('should join a room and receive a join-room event', (done) => {
-        let passcode;
+	test("should join a room and receive a join-room event", (done) => {
+		let passcode;
 
-        client.on('message', (message) => {
-            const event = JSON.parse(message);
-            if (event.type === 'created-room') {
-                passcode = event.passcode;
-                client.send(JSON.stringify({ type: 'join-room', passcode }));
-            } else if (event.type === 'joined-room') {
-                expect(event.id).toBeDefined();
-                done();
-            }
-        });
+		client.on("message", (message) => {
+			const event = JSON.parse(message);
+			if (event.type === "created-room") {
+				passcode = event.passcode;
+				client.send(JSON.stringify({type: "join-room", passcode}));
+			} else if (event.type === "joined-room") {
+				expect(event.id).toBeDefined();
+				done();
+			}
+		});
 
-        client.on('open', () => {
-            client.send(JSON.stringify({ type: 'create-room' }));
-        });
-    });
+		client.on("open", () => {
+			client.send(JSON.stringify({type: "create-room"}));
+		});
+	});
 
-    // test('should send a message to the room and receive a send-message event', (done) => {
-    //     let passcode;
-    //
-    //     client.on('message', (message) => {
-    //         const event = JSON.parse(message);
-    //         if (event.type === 'created-room') {
-    //             passcode = event.passcode;
-    //             client.send(JSON.stringify({ type: 'join-room', passcode }));
-    //         } else if (event.type === 'joined-room') {
-    //             client.send(JSON.stringify({ type: 'room', message: 'Hello, world!' }));
-    //         } else if (event.type === 'room') {
-    //             expect(event.message).toBe('Hello, world!');
-    //             done();
-    //         }
-    //     });
-    //
-    //     client.on('open', () => {
-    //         client.send(JSON.stringify({ type: 'create-room' }));
-    //     });
-    // });
+	// test('should send a message to the room and receive a send-message event', (done) => {
+	//     let passcode;
+	//
+	//     client.on('message', (message) => {
+	//         const event = JSON.parse(message);
+	//         if (event.type === 'created-room') {
+	//             passcode = event.passcode;
+	//             client.send(JSON.stringify({ type: 'join-room', passcode }));
+	//         } else if (event.type === 'joined-room') {
+	//             client.send(JSON.stringify({ type: 'room', message: 'Hello, world!' }));
+	//         } else if (event.type === 'room') {
+	//             expect(event.message).toBe('Hello, world!');
+	//             done();
+	//         }
+	//     });
+	//
+	//     client.on('open', () => {
+	//         client.send(JSON.stringify({ type: 'create-room' }));
+	//     });
+	// });
 });
