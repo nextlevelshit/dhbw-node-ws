9371b8aca07f8792711238fa2f7c9e884ad2035f
Michael Czechowski <m.czechowski@pons.de>
2024-04-14 21:58:37 +0200

    add chat server class add test scripts add jsdoc types extend room class prepare ci tests

--- index.test.js
+++ index.test.js
@@ -1,74 +1,137 @@
-import WebSocket from 'ws';
+import WebSocket from "ws";
+import debug from "debug";
 import {port} from "./src/config/constants";
+import {expect, describe, test, beforeEach, afterEach} from "@jest/globals";
 
-describe('WebSocket server', () => {
-    let client;
+const logger = debug("test:i");
+const verbose = debug("test:v");
 
-    beforeEach(() => {
-        client = new WebSocket(`ws://localhost:${port}`);
-    });
+describe("WebSocket server", () => {
+	let client;
 
-    afterEach(() => {
-        client.close();
-    });
+	beforeEach(() => {
+		client = new WebSocket(`ws://localhost:${port}`);
+	});
 
-    test('should connect to the server', (done) => {
-        client.on('open', () => {
-            done()
-        });
-    });
+	afterEach(() => {
+		client.close();
+	});
 
-    test('should create a room when receiving a create-room event', (done) => {
-        client.on('message', (message) => {
-            const event = JSON.parse(message);
-            if (event.type === 'room-created') {
-                expect(event.id).toBeDefined();
-                done();
-            }
-        });
+	test("should connect to the server", (done) => {
+		client.on("open", () => {
+			done()
+		});
+	});
 
-        client.on('open', () => {
-            client.send(JSON.stringify({ type: 'create-room' }));
-        });
-    });
+	test("should get clientId on connection", (done) => {
+		client.on("message", (message) => {
+			const event = JSON.parse(message);
+			expect(event.type).toBe("connected");
+			expect(event.clientId).toBeDefined();
+			done();
+		});
+	});
 
-    test('should join a room and receive a join-room event', (done) => {
-        let passcode;
+	test("should get message when another user joins", (done) => {
+		client.on("open", () => {
+			client.send(JSON.stringify({type: "create-room"}));
+		});
 
-        client.on('message', (message) => {
-            const event = JSON.parse(message);
-            if (event.type === 'created-room') {
-                passcode = event.passcode;
-                client.send(JSON.stringify({ type: 'join-room', passcode }));
-            } else if (event.type === 'joined-room') {
-                expect(event.id).toBeDefined();
-                done();
-            }
-        });
+		client.on("message", (message) => {
+			const event = JSON.parse(message);
 
-        client.on('open', () => {
-            client.send(JSON.stringify({ type: 'create-room' }));
-        });
-    });
+			if (event.type === "created-room") {
+				expect(event.passcode).toBeDefined();
+				const passcode = event.passcode;
+				const secondClient = new WebSocket(`ws://localhost:${port}`);
+				secondClient.on("open", () => {
+					secondClient.send(JSON.stringify({type: "join-room", passcode}));
+					secondClient.close();
+				});
+			}
 
-    // test('should send a message to the room and receive a send-message event', (done) => {
-    //     let passcode;
-    //
-    //     client.on('message', (message) => {
-    //         const event = JSON.parse(message);
-    //         if (event.type === 'created-room') {
-    //             passcode = event.passcode;
-    //             client.send(JSON.stringify({ type: 'join-room', passcode }));
-    //         } else if (event.type === 'joined-room') {
-    //             client.send(JSON.stringify({ type: 'room', message: 'Hello, world!' }));
-    //         } else if (event.type === 'room') {
-    //             expect(event.message).toBe('Hello, world!');
-    //             done();
-    //         }
-    //     });
-    //
-    //     client.on('open', () => {
-    //         client.send(JSON.stringify({ type: 'create-room' }));
-    //     });
-    // });
+			if (event.type === "user-joined") {
+				expect(event.clientId).toBeDefined();
+				expect(event.clients).toBeGreaterThan(1);
+				done();
+			}
+		});
+	}, 20_000);
+
+	test("should create a room and get room id", (done) => {
+		client.on("message", (message) => {
+			const event = JSON.parse(message);
+			expect(event.type).toBeDefined();
+
+			if (event.type === "created-room") {
+				expect(event.id).toBeDefined();
+				done();
+			}
+		});
+
+		client.on("open", () => {
+			client.send(JSON.stringify({type: "create-room"}));
+		});
+	});
+
+	test("should join a room and receive a create-room event", (done) => {
+		let passcode;
+
+		client.on("message", async (message) => {
+			const event = JSON.parse(message);
+			if (event.type === "created-room") {
+				passcode = event.passcode;
+				await client.send(JSON.stringify({type: "join-room", passcode}));
+			} else if (event.type === "joined-room") {
+				expect(event.id).toBeDefined();
+				done();
+			}
+		});
+
+		client.on("open", () => {
+			client.send(JSON.stringify({type: "create-room"}));
+		});
+	});
+
+	test("should send a message to the room and receive a send-message event", (done) => {
+		client.on("message", async (message) => {
+			const event = JSON.parse(message);
+			expect(event.type).toBeDefined();
+			logger("first client ->", event.type);
+			verbose(event);
+
+			if (event.type === "created-room") {
+				expect(event.id).toBeDefined();
+				expect(event.passcode).toBeDefined();
+				const passcode = event.passcode;
+
+				const secondClient = new WebSocket(`ws://localhost:${port}`)
+				secondClient.on("message", (secondMessage) => {
+					const event = JSON.parse(secondMessage);
+					expect(event.type).toBeDefined();
+					logger("second client ->", event.type);
+					verbose(event);
+
+					if (event.type === "message") {
+						expect(event.message).toBe("Hello, world!");
+						secondClient.close();
+						done();
+					}
+				});
+
+				secondClient.on("open", () => {
+					verbose("second client connected");
+					secondClient.send(JSON.stringify({type: "join-room", passcode}));
+				});
+			} else if (event.type === "user-joined") {
+				expect(event.clients).toBe(2);
+				expect(event.clientId).toBeDefined();
+				client.send(JSON.stringify({type: "message", message: "Hello, world!"}));
+			}
+		});
+
+		client.on("open", () => {
+			client.send(JSON.stringify({type: "create-room"}));
+		});
+	}, 20_000);
 });
--- src/Room.js
+++ src/Room.js
@@ -1,38 +1,120 @@
+import debug from "debug";
+
+const logger = debug("app:i:room");
+const verbose = debug("app:v:room");
+
+/**
+ * Room class represents a chat room.
+ * It manages the clients connected to the room and the room's context.
+ */
 export class Room {
-    constructor() {
-        this.passcode = Math.random().toString(36).substring(2, 6);
-        this.id = Buffer.from(this.passcode).toString("base64");
-        this.context = {};
-        this.clients = new Map();
-    }
+	/**
+	 * The context and game state of the room.
+	 * @type {object}
+	 */
+	context;
+	/**
+	 * The clients connected to the room.
+	 * @type {Map<string, WebSocket & {sendEvent:  (string, object) => void, roomId: string, id: string}>}
+	 */
+	clients;
+	/**
+	 * The four alphanumeric character long passcode for the room.
+	 * @type string
+	 */
+	passcode;
+	/**
+	 * The unique identifier for the room.
+	 * It is generated from the passcode.
+	 * It is used to identify the room in the rooms map.
+	 * It is also used to send messages to the room.
+	 * It is sent to the client when they join the room.
+	 * It is used to leave the room when the client disconnects.
+	 * It is used to broadcast a user-left event to the other clients in the room.
+	 * It is used to create a new room when the client creates a room.
+	 * It is used to join a room when the client joins a room.
+	 * It is used to find the room when the client leaves a room.
+	 * It is used to find the room when the client sends a message to the room.
+	 * It is used to find the room when the client updates the room's context.
+	 * It is used to find the room when the client requests the list of rooms.
+	 * It is used to find the room when the client requests the list of clients in the room.
+	 * It is used to find the room when the client pings the server.
+	 * It is used to find the room when the client sends an invalid event type.
+	 * It is used to find the room when the client sends an error message.
+	 * It is used to find the room when the client sends a message to the room.
+	 * @type {string}
+	 */
+	id;
 
-    join(clientId, ws) {
-        this.clients.set(clientId, ws);
-        if (ws) {
-            ws.send(JSON.stringify({
-                type: "joined-room",
-                clients: this.clients.size,
-                passcode: this.passcode,
-                id: this.id
-            }));
-        }
-    }
+	constructor() {
+		this.passcode = Math.random().toString(36).substring(2, 6).toUpperCase();
+		this.id = Buffer.from(this.passcode).toString("base64");
+		this.context = {};
+		this.clients = new Map();
+		logger(this.id, "new room");
+		verbose({passcode: this.passcode});
+	}
 
-    leave(clientId) {
-        const ws = this.clients.get(clientId);
-        if (ws) {
-            this.clients.delete(clientId);
-            ws.send(JSON.stringify({type: "left-room", passcode: this.passcode, id: this.id}));
-            this.broadcast({type: "user-left", clients: this.clients.size});
-            console.log(`Client ${clientId} left room ${this.id}`);
-        }
-    }
+	/**
+	 * join method is called when a client joins the room.
+	 * It adds the client to the clients map and sends a joined-room event to the client.
+	 * @param {WebSocket & {sendEvent:  (string, object) => void, roomId: string, id: string}} ws - The WebSocket instance for the client.
+	 */
+	join(ws) {
+		const clients = this.clients.set(ws.id, ws);
+		ws.sendEvent("joined-room", {clients: clients.size, passcode: this.passcode, id: this.id});
+		this.broadcast("user-joined", {clientId: ws.id, clients: clients.size}, ws.id);
+		this.pulse(ws);
+		logger("joined room", this.id);
+		verbose(ws.id);
+		verbose({clients: clients.size});
+	}
 
-    broadcast(data) {
-        this.clients.forEach(ws => ws.send(JSON.stringify({...data, context: this.context})));
-    }
+	/**
+	 * leave method is called when a client leaves the room.
+	 * It removes the client from the clients map and sends a left-room event to the client.
+	 * It also broadcasts a user-left event to the other clients in the room.
+	 * @param {string} clientId - The unique identifier for the client.
+	 */
+	leave(clientId) {
+		const ws = this.clients.get(clientId);
+		if (ws) {
+			this.clients.delete(clientId);
+			ws.sendEvent("left-room", {passcode: this.passcode, id: this.id});
+			this.broadcast("user-left", {clientId, clients: this.clients.size}, ws.id);
+			// if (this.clients.size === 0) {
+			// 	logger("closing room", this.id);
+			// 	verbose({id: this.id, clients: this.clients.size});
+			// 	this.clients.clear();
+			// 	delete this;
+			// }
+		}
+	}
 
-    updateContext(update) {
-        this.context = {...this.context, ...update};
-    }
+	/**
+	 * broadcast method is called to send a message to all clients in the room.
+	 * It sends the message to each client in the clients map.
+	 * @param {string} type - The event type
+	 * @param {object} data - The message to send to the clients.
+	 * @param {string} clientId - The unique identifier for the client that sent the message.
+	 */
+	broadcast(type, data, clientId) {
+		[...this.clients.entries()].filter(([id]) => id !== clientId).forEach(([,ws]) => ws.sendEvent(type, {...data, context: this.context}));
+	}
+
+	/**
+	 * updateContext method is called to update the room's context.
+	 * It merges the existing context with the provided update.
+	 * @param {object} update - The update to apply to the context.
+	 */
+	updateContext(update) {
+		this.context = {...this.context, ...update};
+	}
+
+	/**
+	 * @param {WebSocket & {sendEvent:  (string, object) => void, roomId: string, id: string}} ws - The WebSocket instance for the client.
+	 */
+	pulse(ws) {
+		ws.sendEvent("pulse", {context: this.context, clients: [...this.clients.keys()]});
+	}
 }
